71ec8c949adc516f79735ec536593bfe7ff368bb
unknown <avesh@innovatex.in>
2024-04-16 14:46:08 +0530

    Design Changes

---
+++ src/components/pages/checkout/Checkout.jsx
@@ -0,0 +1,223 @@
+import React from "react";
+import Header from "../../common/header/Header";
+import Footer from "../../common/footer/Footer";
+import "./Checkout.css";
+import { MdOutlineRemoveShoppingCart } from "react-icons/md";
+
+function Checkout() {
+  return (
+    <>
+      <link
+        href="../../Assets/shop/t/9/assets/bootstrap.min528d.css?v=19868337340211324411698938058"
+        rel="stylesheet"
+        type="text/css"
+        media="all"
+      />
+      <link
+        href="../../Assets/shop/t/9/assets/stylea2a8.css?v=23287745902246098161698938060"
+        rel="stylesheet"
+        type="text/css"
+        media="all"
+      />
+      <link
+        href="../../Assets/shop/t/9/assets/theme-custom0acb.css?v=63313606314167933051698938089"
+        rel="stylesheet"
+        type="text/css"
+        media="all"
+      />
+      <link
+        href="../../Assets/shop/t/9/assets/skin-and-color85c1.css?v=64489753365287532231698938089"
+        rel="stylesheet"
+        type="text/css"
+        media="all"
+      />
+      <script src="../../Assets/shop/t/9/assets/jquery-1.12.4.min61ad.js?v=167620768852842279821698938059"></script>
+      <script src="../../Assets/shop/t/9/assets/theme08ca.js?v=58729172318499673551698938089"></script>
+      <link
+        href="../../Assets/shop/t/9/assets/icons.min4e41.css?v=144771626144460745771698938059"
+        rel="stylesheet"
+        type="text/css"
+        media="all"
+      />
+      <link
+        href="../../Assets/shop/t/9/assets/icofont.minba8d.css?v=49818375117710154861698938058"
+        rel="stylesheet"
+        type="text/css"
+        media="all"
+      />
+
+      <div id="preloader_active">
+        <div id="loading-center">
+          <div id="loading-center-absolute">
+            <div className="object" id="object_four" />
+            <div className="object" id="object_three" />
+            <div className="object" id="object_two" />
+            <div className="object" id="object_one" />
+          </div>
+        </div>
+      </div>
+      <div id="shopify-section-header" className="shopify-section">
+        <div style={{ height: "16vh" }}>
+          <Header />
+        </div>
+      </div>
+      <div className="breadcrumb-area breadcrumbs-section">
+        <div className="breadcrumbs overlay-bg">
+          <div className="container">
+            <div className="breadcrumb-content text-center breadcrumbs-inner">
+              <div className="page-title">
+                <h1
+                  className="breadcrumbs-title"
+                  style={{ textTransform: "unset" }}
+                >
+                  Check Out
+                </h1>
+              </div>
+              <nav>
+                <ul className="breadcrumb-list">
+                  <li>
+                    <a href="index.html" title="Back to the home page">
+                      Home
+                    </a>
+                  </li>
+                  <li style={{ textTransform: "unset" }}>Check Out</li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="shopify-section-breadcrumb" className="shopify-section"></div>
+      <main>
+        <br />
+        <div
+          id="shopify-section-template--14595761635408__main"
+          className="shopify-section"
+        >
+          <div className="contact-area pt-100 pb-100">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-6 col-md-6">
+                <form
+                      method="post"
+                      action=""
+                      id="contact-form"
+                      acceptCharset="UTF-8"
+                      className="contact-form"
+                    >
+                      <input
+                        type="hidden"
+                        name="form_type"
+                        defaultValue="contact"
+                      />
+                      <input type="hidden" name="utf8" defaultValue="✓" />
+                      <input
+                        type="text"
+                        placeholder="Name"
+                        className=""
+                        name="contact[name]"
+                        id="ContactFormName"
+                        defaultValue=""
+                      />
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className=""
+                        name="contact[email]"
+                        id="ContactFormEmail"
+                        defaultValue=""
+                      />
+                      <input
+                        type="text"
+                        id="ContactFormSubject"
+                        name="contact[subject]"
+                        placeholder="Subject"
+                        defaultValue=""
+                      />
+                      <textarea
+                        rows={2}
+                        placeholder="Your Message"
+                        name="contact[body]"
+                        id="ContactFormMessage"
+                        style={{ height: "auto" }}
+                      />
+                      <button className="submit" type="submit">
+                        Send Message
+                      </button>
+                    </form>
+                </div>
+                <div className="col-lg-6 col-md-6">
+                  <table className="table table-spriped">
+                    <tr>
+                      <th style={{ fontSize: "18px", width: "20%" }}>
+                        Product
+                      </th>
+                      <th>Description</th>
+                      <th style={{ textAlign: "right", fontSize: "18px" }}>
+                        Qty
+                      </th>
+                      <th style={{ textAlign: "right", fontSize: "18px" }}>
+                        Price
+                      </th>
+                      <th style={{ textAlign: "right", fontSize: "18px" }}>
+                        Amount
+                      </th>
+                    </tr>
+                    <tr>
+                      <td>
+                        <img
+                          src="Assets/shop/collections/2d8ec.png?v=1698470765"
+                          alt="Best Sellers"
+                          loading="lazy"
+                          style={{ height: "10vh" }}
+                        />
+                      </td>
+                      <td style={{ fontWeight: "600", fontSize: "18px" }}>
+                        Book Name Here
+                      </td>
+                      <td style={{ textAlign: "right" }}>50</td>
+                      <td style={{ textAlign: "right" }}>Rs. 100</td>
+                      <td style={{ textAlign: "right" }}>Rs. 5000</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <img
+                          src="Assets/shop/collections/p880c1.png?v=1698470737"
+                          alt="Best Sellers"
+                          loading="lazy"
+                          style={{ height: "10vh" }}
+                        />
+                      </td>
+                      <td style={{ fontWeight: "600", fontSize: "18px" }}>
+                        Book Name Here
+                      </td>
+                      <td style={{ textAlign: "right" }}>20</td>
+                      <td style={{ textAlign: "right" }}>Rs. 200</td>
+                      <td style={{ textAlign: "right" }}>Rs. 4000</td>
+                    </tr>
+                    <tr>
+                      <th>Total</th>
+                      <th></th>
+                      <th style={{ textAlign: "right" }}>70</th>
+                      <th style={{ textAlign: "right" }}>Rs. 300</th>
+                      <th style={{ textAlign: "right" }}>Rs. 9000</th>
+                    </tr>
+                  </table>
+                  <br></br>
+                  <center>
+                    <a className="ban_btn1 banner_style_2" href="index.html">
+                      Place Your Order Now
+                    </a>
+                  </center>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Checkout;
