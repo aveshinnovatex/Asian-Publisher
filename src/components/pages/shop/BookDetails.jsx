bb28cb35845e564fbaf52e152c349c51e7cc7e86
unknown <avesh@innovatex.in>
2024-04-16 17:31:33 +0530

    Design Changes

--- src/components/pages/shop/BookDetails.jsx
+++ src/components/pages/shop/BookDetails.jsx
@@ -101,24 +101,254 @@ function Shop() {
           className="shopify-section"
         >
           <div
-            className="shop-area pt-95 pb-100 section-padding-3"
+            className="shop-area pt-20 pb-20 section-padding-3"
             id="section-template--14595761406032__main"
           >
             <div className="container">
               <div className="row">
                 <div className="col-lg-12">
-                  <div className="shop-bottom-area mt-35">
+                  <div className="shop-bottom-area">
                     <div className="tab-content jump">
-                      <div>
-                        <div className="row theme-products ">
-                        <div className="col-lg-6" style={{backgroundImage: "url(../Assets/shop/collections/2d8ec.png?v=1698470765)", height:"80vh", }}>
-    </div>
+                      <div
+                        style={{
+                          boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.8)",
+                        }}
+                      >
+                        <div className="row theme-products">
+                          <div
+                            className="col-lg-1"
+                            style={{
+                              float: "left",
+                            }}
+                          ></div>
+                          <div
+                            className="col-lg-10"
+                            style={{
+                              float: "left",
+                            }}
+                          >
+                            <div
+                              className="col-lg-6"
+                              style={{
+                                float: "left",
+                                backgroundImage:
+                                  "url(../Assets/shop/collections/2d8ec.png?v=1698470765)",
+                                height: "100vh",
+                                backgroundPosition: "center",
+                                backgroundSize: "cover",
+                              }}
+                            ></div>
+                            <div
+                              className="col-lg-1"
+                              style={{
+                                float: "left",
+                                paddingTop: "10px 10px 10px 10px",
+                              }}
+                            ></div>
+                            <div
+                              className="col-lg-5"
+                              style={{
+                                float: "left",
+                                padding: "50px 10px 10px 10px",
+                              }}
+                            >
+                              <h3 style={{ fontWeight: "600" }}>
+                                Book Name Here
+                              </h3>
+                              <h6>ISBN: 978-93-5502-231-8</h6>
+                              <p style={{ color: "red", fontWeight: "600" }}>
+                                Rs. 300
+                              </p>
+                              {/* <p style={{color:"#000", fontWeight:"600", fontSize:"20px"}}>Book Category :</p> */}
+
+                              <table className="table table-spriped">
+                                <tr>
+                                  <th>Sr. No.</th>
+                                  <th>Category</th>
+                                </tr>
+                                <tr>
+                                  <td>1.</td>
+                                  <td>Test</td>
+                                </tr>
+                              </table>
+
+                              <center>
+                                <div
+                                  className="col-lg-6"
+                                  style={{ float: "left", marginTop: "7px" }}
+                                >
+                                  <select className="form-control">
+                                    <option selected>Select Language</option>
+                                    <option>Hindi</option>
+                                    <option>English</option>
+                                  </select>
+                                </div>
+                                <div
+                                  className="col-lg-6"
+                                  style={{ float: "left" }}
+                                >
+                                  <div
+                                    className="quantity"
+                                    style={{ width: "80%" }}
+                                  >
+                                    <button
+                                      className="minus"
+                                      aria-label="Decrease"
+                                    >
+                                      −
+                                    </button>
+                                    <input
+                                      type="number"
+                                      className="input-box"
+                                      defaultValue={1}
+                                      min={1}
+                                      max={10}
+                                      style={{ width: "100%" }}
+                                    />
+                                    <button
+                                      className="plus"
+                                      aria-label="Increase"
+                                    >
+                                      +
+                                    </button>
+                                  </div>
+                                </div>
+                              </center>
+
+                              <br></br>
+                              <br></br>
+                              <br></br>
+                              <center>
+                                <a
+                                  className="ban_btn1 banner_style_2"
+                                  href="index.html"
+                                >
+                                  Add To Cart
+                                </a>
+                              </center>
+                            </div>
+                          </div>
+                          <div
+                            className="col-lg-1"
+                            style={{
+                              float: "left",
+                            }}
+                          ></div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="row" style={{ padding: "30px" }}>
+            <div
+              style={{
+                backgroundColor: "#F6F2E7",
+                padding: "20px",
+                width: "100%",
+                height: "12vh",
+              }}
+            >
+              <h3>Shipping & Refund</h3>
+              <br></br>
+            </div>
+            <div
+              style={{ backgroundColor: "#fff", padding: "50px 50px 5px 50px" }}
+            >
+              <b>Shipping policy</b>
+
+              <p>
+                We offer shipping services to all parts of India, even remote
+                areas, and ship six days a week. Orders received after 12 p.m.
+                IST on any given day are processed the next day. On Sundays and
+                national holidays, we do not process orders. We also accept
+                orders for supplies on demand. If you require a book
+                immediately, we can deliver it to you within 24 hours for an
+                additional fee. Our normal shipping time ranges between 6 and 8
+                days. When delivering to a remote location, delivery times may
+                vary depending on the area or location of the order. If none of
+                our shipping partners deliver to your area, we can send your
+                order via the India Post package. Orders delivered by India Post
+                cannot be tracked, and in some situations, the parcel may take
+                up to 10-15 days to arrive.
+              </p>
+
+              <b>Track your order</b>
+              <p>
+                Visit our delivery partner's website to follow your order using
+                the tracking number supplied in your order confirmation email.
+                It could take up to 12-24 hours for your tracking number to be
+                updated on our website.
+              </p>
+              <b>Return and Exchange policy</b>
+
+              <p>
+                All orders placed with us are guaranteed 7 Day Replacement. We
+                use the best packaging and shipping methods to ensure that the
+                orders reach their intended destination in good condition. We do
+                not take any responsibility for the goods which are damaged
+                during the transit. If you have received a damaged or defective
+                book, please contact us within 14 days of your order date to get
+                a replacement. You can also get your order replaced once without
+                any additional charge. You can cancel your order online up to 12
+                hours before the book is shipped. In this case, the entire
+                amount will be refunded. If the payment is made via a
+                debit/credit card, the money will be refunded to the card
+                itself. Once the order has been cancelled it will take 8-10
+                working days to process the refund. If you paid with a DD, then
+                you can send us an e-mail or call us to notify us of your bank
+                number. We will make sure that your refund reaches you ASAP
+              </p>
+            </div>
+          </div>
+
+          <div className="row" style={{ padding: "30px" }}>
+            <div
+              style={{
+                backgroundColor: "#F6F2E7",
+                padding: "20px",
+                width: "100%",
+                height: "12vh",
+              }}
+            >
+              <h3>Asian Publishers Benefits</h3>
+              <br></br>
+            </div>
+            <div
+              style={{ backgroundColor: "#fff", padding: "50px 50px 5px 50px" }}
+            >
+              <p>
+                Asian publishers aim to help its students through all the
+                regions with its simplistic and comprehensive study methods. Our
+                books teach our students with the virtue of providing-
+                <br></br>
+                <b>• VARIETY OF EXAMPLES :</b> Each chapter of our book is
+                designed to house multiple examples from different perspectives
+                so that everyone can understand in accordance with their style
+                of learning.
+                <br></br>
+                <b>• MULTIPLE LANGUAGES :</b> The content and difficult concepts
+                are explained in multiple languages so that students can
+                understand them with ease.
+                <br></br>
+                <b>• SUMMARIES :</b> Authors have incorporated comprehensive
+                summaries at the end of every chapter so that students can go
+                over their syllabus quickly.
+                <br></br>
+                <b>• STUDY RESOURCES :</b> Our books house the ever-precious
+                banks of questions and resources for revision which help the
+                students effectively prepare for exams.
+                <br></br>
+                <b>• COMPETITIVE PRICES :</b> Our books are widely and easily
+                accessible at competitive market prices so that each and every
+                person can make effective use of them.
+                <br></br>
+                <b>• COVERS ENTIRE SYLLABUS :</b> Our books are designed to
+                cover the entire syllabus of the UP technical board so that our
+                students need not look outside for additional resources.
+              </p>
             </div>
           </div>
         </div>
